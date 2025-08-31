6bf07c7e788ca26da298be4aa12bd03a5a8dcf60
Carlos Cortón Cobas <104267232+carloscortonc@users.noreply.github.com>
2025-08-31 15:48:50 +0200

    build: configure de-env with docker-compose

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
-import mkcert from 'vite-plugin-mkcert'
 import svgr from "vite-plugin-svgr";
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
+    host: true,
+    // Do this so proxy works from inside docker container
+    allowedHosts: true,
   },
-  plugins: [svgr({include: "**/*.svg"}),react(), tsconfigPaths(),  mkcert()],
+  plugins: [svgr({ include: "**/*.svg" }), react(), tsconfigPaths()],
 });
